1259767bfabe482e756249d189b314b6fede05fa
Peretz83 <liavperetz83@gmail.com>
2022-08-17 01:34:15 +0300

    fixed links

--- JS/index.js
+++ JS/index.js
@@ -2,5 +2,7 @@ let elem = document.getElementById('elem')
 
 elem.addEventListener('click', ()=>{
 
-  location.href="http://127.0.0.1:5500/form/indexform.html"
+  location.href="http://127.0.0.1:5500/mardi-gras/contact.html"
 })
+
+// fjfjfjf
